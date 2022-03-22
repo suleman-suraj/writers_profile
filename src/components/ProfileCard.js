005770d894e7f3408558fd138ccd2365126fec36
suleman-suraj <sulemansuraj1233@gmail.com>
2022-03-22 11:29:38 +0000

    reactevents

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,21 +1,30 @@
 import React from 'react'
-//import {useState} from "react"
+import {useState} from "react"
+
 function ProfileCard({writer}) {
-  // const [showBio, setShowBio] = useState(false);
-    //const handleClick = (bioData) =>{
-     //   setShowBio(!showBio);
-   // }
+    const [showBio, setShowBio] = useState(false);
+    const handleClick = (bioData) =>{
+        setShowBio(!showBio);
+    }
+
     return (
         <div className='card'>
+        <div className='carContent'>
+            {showBio ? (
+            <div>
+            <p className='bio'>{writer.bio}</p>
+            </div>
+            ):(
             <img src= {`images/${writer.avatar}.png`}
-            height="300px" width="300px" alt={writer.img}/>
-
+            height="300px" width="300px" alt={writer.img}/> )}
+            </div>
+            
             <div className='textGroup'>
                 <h3>{writer.name}</h3>
                 <p>{writer.email}</p>
                 <p>{writer.phone}</p>
 
-                <button className='actionBtn'>Read Bio</button>
+                <button className='actionBtn'onClick={()=>handleClick(writer.bio)}>Read Bio</button>
             </div>
         </div>
     )
